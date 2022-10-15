48dd3b38281ae487ce7e92233c02bf02da8e3b81
Enzo Vieira <enzovieira16@hotmail.com>
2022-10-15 23:46:29 +0100

    add: Button and Input

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,27 @@
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
+
+import { styles } from "./styles";
+
+export function Home() {
+  const handleParticipantAdd = () => {
+    console.log("");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6B6B6B"
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#131016",
+    padding: 24,
+  },
+  eventName: {
+    color: "#FDFCFE",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+  eventDate: {
+    color: "#6B6B6B",
+    fontSize: 16,
+  },
+  input: {
+    height: 56,
+    backgroundColor: "#1F1E25",
+    borderRadius: 5,
+    color: "#FBFCFE",
+    padding: 16,
+    fontSize: 16,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: "#31CF67",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 24,
+  },
+});
