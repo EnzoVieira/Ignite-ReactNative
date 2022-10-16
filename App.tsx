215187473c2da9b0eb3f26e00049da5186ff89d4
Enzo Vieira <enzovieira16@hotmail.com>
2022-10-16 01:40:21 +0100

    add: StatusBar style

--- App.tsx
+++ App.tsx
@@ -1,5 +1,16 @@
+import { StatusBar } from "react-native";
 import { Home } from "./src/screens/Home";
 
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      <Home />
+    </>
+  );
 }
