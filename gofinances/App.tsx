4311346cd391e81d13414e517731ebe026b0203b
Enzo Vieira <enzovieira16@hotmail.com>
2022-10-23 22:49:04 +0100

    add(gofinances): Dashboard Header

--- gofinances/App.tsx
+++ gofinances/App.tsx
@@ -1,21 +1,32 @@
-import { StatusBar } from "expo-status-bar"
-import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { ThemeProvider } from "styled-components"
+import * as SplashScreen from "expo-splash-screen"
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins"
+
+import theme from "./src/global/styles/theme"
+
+import { Dashboard } from "./src/screens/Dashboard"
+
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  })
+
+  if (fontsLoaded) {
+    SplashScreen.hideAsync()
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-})
---
+++ gofinances/src/screens/Dashboard/index.tsx
@@ -0,0 +1,45 @@
+import { HighlightCard } from "../../components/HighlightCard"
+
+import {
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  UserWrapper,
+  Icon,
+  HighlightCards,
+} from "./styles"
+
+export const Dashboard = () => {
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/73349819?v=4",
+              }}
+            />
+
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Enzo</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+
+      <HighlightCards>
+        <HighlightCard />
+        <HighlightCard />
+        <HighlightCard />
+      </HighlightCards>
+    </Container>
+  )
+}
